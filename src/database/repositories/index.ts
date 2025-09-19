20c1d2c7c37cf9c82eb934beed3bfccdd3634698
Balti Solanilla <solanilla.baltasar@gmail.com>
2025-09-20 09:36:35 +1000

    Add removal history repository

--- src/database/repositories/index.ts
+++ src/database/repositories/index.ts
@@ -2,6 +2,7 @@ export { blacklistRepository } from './blacklistRepository';
 export { groupMembershipRepository } from './groupMembershipRepository';
 export { groupRepository } from './groupRepository';
 export { messageRepository } from './messageRepository';
+export { removalHistoryRepository } from './removalHistoryRepository';
 export { removalQueueRepository } from './removalQueueRepository';
 export { userRepository } from './userRepository';
 export { webhookEventRepository } from './webhookEventRepository';
