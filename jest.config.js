b701895aad45463e0b42c9e89a02f2320568fd8b
Balti Solanilla <solanilla.baltasar@gmail.com>
2025-09-26 20:45:53 +1000

    Remove module mapper for the time being

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,3 @@ export const preset = 'ts-jest';
 export const testEnvironment = 'node';
 export const testMatch = ['**/?(*.)+(spec|test).[jt]s?(x)'];
 export const moduleFileExtensions = ['ts', 'js', 'json'];
-export const moduleNameMapper = {
-	'^@logic/(.*)$': '<rootDir>/src/logic/$1',
-	'^@routes/(.*)$': '<rootDir>/src/routes/$1',
-	'^@services/(.*)$': '<rootDir>/src/services/$1',
-	'^@database/(.*)$': '<rootDir>/src/database/$1',
-	'^@config$': '<rootDir>/src/config',
-	'^@constants/(.*)$': '<rootDir>/src/constants/$1',
-	'^@utils/(.*)$': '<rootDir>/src/utils/$1',
-	'^@types/(.*)$': '<rootDir>/src/types/$1'
-};
